994de2c8d363e8ce97f549c9c86e17ab2182f66c
jimbo23 <kiefersoon@gmail.com>
2023-06-05 21:56:23 +0800

    responsive fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen p-24">
-      <div className="grid grid-cols-3 gap-10 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-full">
         {images?.map((image) => (
           <BlurImage image={image} key={image.imageSrc} />
         ))}
